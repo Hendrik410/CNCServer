3a595fe1558d4835debb65d2568d788a5d4f3238
Hendrik410 <h.mueller@overzone-studio.de>
2015-04-28 09:01:17 +0200

    removed 'throws Exception' from main

--- CNCServer.java
+++ CNCServer.java
@@ -10,16 +10,20 @@ import java.net.InetSocketAddress;
 public class CNCServer {
     public static CNCController cncController;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
         System.out.println("CNCServer.main");
 
         cncController = new CNCController();
+        try{
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.createContext("/cncc", new CNCHttpHandler());
+            server.createContext("/ajax", new CNCAjaxHandler(cncController));
+            server.setExecutor(null);
+            server.start();
+            System.out.println("HttpServer started");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/cncc", new CNCHttpHandler());
-        server.createContext("/ajax", new CNCAjaxHandler(cncController));
-        server.setExecutor(null);
-        server.start();
-        System.out.println("HttpServer started");
     }
 }
